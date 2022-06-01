760b83e1851b6e826320f9b3e622af4a057807d6
Shin <cu7898@gmail.com>
2022-06-01 15:15:59 +0900

    Ref Remove unused code

--- src/main/common/if_null_return_default_value.js
+++ src/main/common/if_null_return_default_value.js
@@ -10,7 +10,3 @@ function if_null_return_default_value(x) {
 
 export default if_null_return_default_value;
 
-console.log(if_null_return_default_value('hola'));    // hola
-console.log(if_null_return_default_value('hola'));    // hola
-console.log(if_null_return_default_value(null));      // Hey, I'm default value
-console.log(if_null_return_default_value(undefined)); // Hey, I'm default value
